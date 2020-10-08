e0f8a7dfd1a150fbd52c730119ecac6cf1fc1e01
cefect <47092034+cefect@users.noreply.github.com>
2020-10-08 15:09:47 -0600

    Update Q.py

--- hp/Q.py
+++ hp/Q.py
@@ -75,12 +75,12 @@ class Qproj(Basic):
             feedback = MyFeedBackQ()
         self.feedback = feedback
             
-        self.qap = self.init_qgis()
+        self.qap = self._init_qgis()
         self.qproj = QgsProject.instance()
 
-        self.init_algos()
+        self._init_algos()
         
-        self.set_vdrivers()
+        self._set_vdrivers()
         
         self.mstore = QgsMapLayerStore() #build a new map store
         
@@ -103,13 +103,13 @@ class Qproj(Basic):
         return
     
     
-    def init_qgis(self, #instantiate qgis
+    def _init_qgis(self, #instantiate qgis
                   gui = False): 
         """
         WARNING: need to hold this app somewhere. call in the module you're working in (scripts)
         
         """
-        log = self.logger.getChild('init_qgis')
+        log = self.logger.getChild('_init_qgis')
         
         try:
             
@@ -128,13 +128,13 @@ class Qproj(Basic):
         except:
             raise Error('QGIS failed to initiate')
         
-    def init_algos(self): #initiilize processing and add providers
+    def _init_algos(self): #initiilize processing and add providers
         """
         crashing without raising an Exception
         """
     
     
-        log = self.logger.getChild('init_algos')
+        log = self.logger.getChild('_init_algos')
         
         if not isinstance(self.qap, QgsApplication):
             raise Error('qgis has not been properly initlized yet')
@@ -154,7 +154,7 @@ class Qproj(Basic):
     
     
 
-    def set_vdrivers(self):
+    def _set_vdrivers(self):
         
         #build vector drivers list by extension
         """couldnt find a good built-in to link extensions with drivers"""
@@ -343,6 +343,219 @@ class Qproj(Basic):
         res_d = processing.run(algo_nm, ins_d, feedback=self.feedback)
         
         return
+    
+    
+    def selectbylocation(self, #select features (from main laye) by geoemtric relation with comp_vlay
+                vlay, #vlay to select features from
+                comp_vlay, #vlay to compare 
+                
+                result_type = 'select',
+                
+                method= 'new',  #Modify current selection by
+                pred_l = ['intersect'],  #list of geometry predicate names
+                
+                selected_only = False, #selected features only on the comp_vlay
+                
+                #expectations
+                allow_none = False,
+                
+                logger = None,
+
+                ):
+        
+        #=======================================================================
+        # setups and defaults
+        #=======================================================================
+        if logger is None: logger=self.logger    
+        algo_nm = 'native:selectbylocation'   
+        log = logger.getChild('selectbylocation')
+        
+        #===========================================================================
+        # #set parameter translation dictoinaries
+        #===========================================================================
+        meth_d = {'new':0}
+            
+        pred_d = {
+                'are within':6,
+                'intersect':0,
+                'overlap':5,
+                  }
+        
+        #predicate (name to value)
+        pred_l = [pred_d[pred_nm] for pred_nm in pred_l]
+        
+        if selected_only:
+            intersect = self._get_sel_obj(comp_vlay)
+        else:
+            intersect = comp_vlay
+    
+        #=======================================================================
+        # setup
+        #=======================================================================
+        ins_d = { 
+            'INPUT' : vlay, 
+            'INTERSECT' : comp_vlay, 
+            'METHOD' : meth_d[method], 
+            'PREDICATE' : pred_l }
+        
+        log.debug('executing \'%s\' on \'%s\' with: \n     %s'
+            %(algo_nm, vlay.name(), ins_d))
+            
+        #===========================================================================
+        # #execute
+        #===========================================================================
+        _ = processing.run(algo_nm, ins_d,  feedback=self.feedback)
+        
+        
+        #=======================================================================
+        # check
+        #=======================================================================
+        fcnt = vlay.selectedFeatureCount()
+        
+        if fcnt == 0:
+            msg = 'No features selected!'
+            if allow_none:
+                log.warning(msg)
+            else:
+                raise Error(msg)
+            
+        #=======================================================================
+        # wrap
+        #=======================================================================
+        log.debug('selected %i (of %i) features from %s'
+            %(vlay.selectedFeatureCount(),vlay.dataProvider().featureCount(), vlay.name()))
+        
+        return self._get_sel_res(vlay, result_type=result_type, logger=log, allow_none=allow_none)
+        
+    def saveselectedfeatures(self,#generate a memory layer from the current selection
+                             vlay,
+                             logger=None,
+                             allow_none = False,
+                             layname=None): 
+        
+        
+        
+        #===========================================================================
+        # setups and defaults
+        #===========================================================================
+        if logger is None: logger = self.logger
+        log = logger.getChild('saveselectedfeatures')
+        algo_nm = 'native:saveselectedfeatures'
+        
+        if layname is None: 
+            layname = '%s_sel'%vlay.name()
+              
+        #=======================================================================
+        # precheck
+        #=======================================================================
+        fcnt = vlay.selectedFeatureCount()
+        if fcnt == 0:
+            msg = 'No features selected!'
+            if allow_none:
+                log.warning(msg)
+                return None
+            else:
+                raise Error(msg)
+        
+        log.debug('on \'%s\' with %i feats selected'%(
+            vlay.name(), vlay.selectedFeatureCount()))
+        #=======================================================================
+        # # build inputs
+        #=======================================================================
+        ins_d = {'INPUT' : vlay,
+                 'OUTPUT' : 'TEMPORARY_OUTPUT'}
+        
+        log.debug('\'native:saveselectedfeatures\' on \'%s\' with: \n   %s'
+            %(vlay.name(), ins_d))
+        
+        #execute
+        res_d = processing.run(algo_nm, ins_d,  feedback=self.feedback)
+
+        
+        res_vlay = res_d['OUTPUT']
+        
+        assert isinstance(res_vlay, QgsVectorLayer)
+        #===========================================================================
+        # wrap
+        #===========================================================================
+
+        res_vlay.setName(layname) #reset the name
+
+        return res_vlay
+    
+    def _get_sel_obj(self, vlay): #get the processing object for algos with selections
+        
+        log = self.logger.getChild('_get_sel_obj')
+        
+        if vlay.selectedFeatureCount() == 0:
+            raise Error('Nothing selected. exepects some pre selection')
+        
+        """consider moving this elsewhere"""
+        #handle project layer store
+        if QgsProject.instance().mapLayer(vlay.id()) is None:
+            #layer not on project yet. add it
+            if QgsProject.instance().addMapLayer(vlay, False) is None:
+                raise Error('failed to add map layer \'%s\''%vlay.name())
+
+        
+       
+        log.debug('based on %i selected features from \'%s\': %s'
+                  %(len(vlay.selectedFeatureIds()), vlay.name(), vlay.selectedFeatureIds()))
+            
+        return QgsProcessingFeatureSourceDefinition(vlay.id(), True)
+    
+    
+    def _get_sel_res(self, #handler for returning selection like results
+                        vlay, #result layer (with selection on it
+                         result_type='select',
+                         
+                         #expectiions
+                         allow_none = False,
+                         logger=None
+                         ):
+        
+        #=======================================================================
+        # setup
+        #=======================================================================
+        if logger is None: logger = self.logger
+        log = logger.getChild('_get_sel_res')
+        #=======================================================================
+        # precheck
+        #=======================================================================
+        if vlay.selectedFeatureCount() == 0:
+            if not allow_none:
+                raise Error('nothing selected')
+            
+            return None
+
+        
+        #log.debug('user specified \'%s\' for result_type'%result_type)
+        #=======================================================================
+        # by handles
+        #=======================================================================
+        if result_type == 'select':
+            #log.debug('user specified \'select\', doing nothing with %i selected'%vlay.selectedFeatureCount())
+            
+            result = None
+            
+        elif result_type == 'fids':
+            
+            result = vlay.selectedFeatureIds() #get teh selected feature ids
+            
+        elif result_type == 'feats':
+            
+            result =  {feat.id(): feat for feat in vlay.getSelectedFeatures()}
+            
+            
+        elif result_type == 'layer':
+            
+            result = self.saveselectedfeatures(vlay, logger=log)
+            
+        else: 
+            raise Error('unexpected result_type kwarg')
+            
+        return result
+   
         
         
 class MyFeedBackQ(QgsProcessingFeedback):
@@ -429,7 +642,208 @@ class MyFeedBackQ(QgsProcessingFeedback):
         self.setProgress(prog)
         
 
+#===============================================================================
+# standalone funcs--------
+#===============================================================================
 
+def vlay_get_fdf( #pull all the feature data and place into a df
+                    vlay,
+                    fmt='df', #result fomrat key. 
+                        #dict: {fid:{fieldname:value}}
+                        #df: index=fids, columns=fieldnames
+                    
+                    #limiters
+                    request = None, #request to pull data. for more customized requestes.
+                    fieldn_l = None, #or field name list. for generic requests
+                    
+                    #modifiers
+                    reindex = None, #optinal field name to reindex df by
+                    
+                    #expectations
+                    expect_all_real = False, #whether to expect all real results
+                    allow_none = False,
+                    
+                    db_f = False,
+                    logger=mod_logger,
+                    feedback=MyFeedBackQ()):
+    """
+    performance improvement
+    
+    Warning: requests with getFeatures arent working as expected for memory layers
+    
+    this could be combined with vlay_get_feats()
+    also see vlay_get_fdata() (for a single column)
+    
+    
+    RETURNS
+    a dictionary in the Qgis attribute dictionary format:
+        key: generally feat.id()
+        value: a dictionary of {field name: attribute value}
+
+    """
+    #===========================================================================
+    # setups and defaults
+    #===========================================================================
+    log = logger.getChild('vlay_get_fdf')
+    
+    assert isinstance(vlay, QgsVectorLayer)
+    all_fnl = [fieldn.name() for fieldn in vlay.fields().toList()]
+    
+    if fieldn_l is None: #use all the fields
+        fieldn_l = all_fnl
+        
+    else:
+        #TODO add field  name check
+        pass        
+
+    
+    if allow_none:
+        if expect_all_real:
+            raise Error('cant allow none and expect all reals')
+        
+    
+    #===========================================================================
+    # prechecks
+    #===========================================================================
+    if not reindex is None:
+        if not reindex in fieldn_l:
+            raise Error('requested reindexer \'%s\' is not a field name'%reindex)
+    
+    if not vlay.dataProvider().featureCount()>0:
+        raise Error('no features!')
+
+    if len(fieldn_l) == 0:
+        raise Error('no fields!')
+    
+    if fmt=='dict' and not (len(fieldn_l)==len(all_fnl)):
+        raise Error('dict results dont respect field slicing')
+    
+    assert hasattr(feedback, 'setProgress')
+    
+    #===========================================================================
+    # build the request
+    #===========================================================================
+    feedback.setProgress(2)
+    if request is None:
+        """WARNING: this doesnt seem to be slicing the fields.
+        see Alg().deletecolumns()
+            but this will re-key things
+        
+        request = QgsFeatureRequest().setSubsetOfAttributes(fieldn_l,vlay.fields())"""
+        request = QgsFeatureRequest()
+        
+    #never want geometry   
+    request = request.setFlags(QgsFeatureRequest.NoGeometry) 
+               
+
+    log.debug('extracting data from \'%s\' on fields: %s'%(vlay.name(), fieldn_l))
+    #===========================================================================
+    # loop through each feature and extract the data
+    #===========================================================================
+    
+    fid_attvs = dict() #{fid : {fieldn:value}}
+    fcnt = vlay.dataProvider().featureCount()
+
+    for indxr, feat in enumerate(vlay.getFeatures(request)):
+        
+        #zip values
+        fid_attvs[feat.id()] = feat.attributes()
+        
+        feedback.setProgress((indxr/fcnt)*90)
+
+
+    #===========================================================================
+    # post checks
+    #===========================================================================
+    if not len(fid_attvs) == vlay.dataProvider().featureCount():
+        log.debug('data result length does not match feature count')
+
+        if not request.filterType()==3: #check if a filter fids was passed
+            """todo: add check to see if the fiter request length matches tresult"""
+            raise Error('no filter and data length mismatch')
+        
+    #check the field lengthes
+    if not len(all_fnl) == len(feat.attributes()):
+        raise Error('field length mismatch')
+
+    #empty check 1
+    if len(fid_attvs) == 0:
+        log.warning('failed to get any data on layer \'%s\' with request'%vlay.name())
+        if not allow_none:
+            raise Error('no data found!')
+        else:
+            if fmt == 'dict': 
+                return dict()
+            elif  fmt == 'df':
+                return pd.DataFrame()
+            else:
+                raise Error('unexpected fmt type')
+            
+    
+    #===========================================================================
+    # result formatting
+    #===========================================================================
+    log.debug('got %i data elements for \'%s\''%(
+        len(fid_attvs), vlay.name()))
+    
+    if fmt == 'dict':
+        
+        return fid_attvs
+    elif fmt=='df':
+        
+        #build the dict
+        
+        df_raw = pd.DataFrame.from_dict(fid_attvs, orient='index', columns=all_fnl)
+        
+        
+        #handle column slicing and Qnulls
+        """if the requester worked... we probably  wouldnt have to do this"""
+        df = df_raw.loc[:, tuple(fieldn_l)].replace(NULL, np.nan)
+        
+        feedback.setProgress(95)
+        
+        if isinstance(reindex, str):
+            """
+            reindex='zid'
+            view(df)
+            """
+            #try and add the index (fids) as a data column
+            try:
+                df = df.join(pd.Series(df.index,index=df.index, name='fid'))
+            except:
+                log.debug('failed to preserve the fids.. column already there?')
+            
+            #re-index by the passed key... should copy the fids over to 'index
+            df = df.set_index(reindex, drop=True)
+            
+            log.debug('reindexed data by \'%s\''%reindex)
+            
+        return df
+    
+    else:
+        raise Error('unrecognized fmt kwarg')
+
+    
+    
+
+def view(#view the vector data (or just a df) as a html frame
+        obj, logger=mod_logger,
+        #**gfd_kwargs, #kwaqrgs to pass to vlay_get_fdatas() 'doesnt work well with the requester'
+        ):
+    
+    if isinstance(obj, pd.DataFrame) or isinstance(obj, pd.Series):
+        df = obj
+    elif isinstance(obj, QgsVectorLayer):
+        """this will index the viewed frame by fid"""
+        df = vlay_get_fdf(obj)
+    else:
+        raise Error('got unexpected object type: %s'%type(obj))
+    
+    basic.view(df)
+    
+    logger.info('viewer closed')
+    
+    return
     
     
 if __name__ == '__main__':
