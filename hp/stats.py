a88838bb760923ecc88a7046fb9ae9cc0d534408
cefect <47092034+cefect@users.noreply.github.com>
2021-12-19 23:33:34 +0100

    Update stats.py

--- hp/stats.py
+++ hp/stats.py
@@ -15,11 +15,69 @@ import sklearn
 import statsmodels.api as sm
 
 
-from hp.plot import Plotr
+from hp.plot import Plotr, view
 
 class SimpleRegression(Plotr):
     
     
+
+    def plot_lm(self, xser, res_d, aName, plotkwargs, pfunc, 
+                ax=None):
+        #=======================================================================
+        # defaults
+        #=======================================================================
+        plt=self.plt
+        if ax is None:
+            ax = plt.gca()
+        
+        #=======================================================================
+        # plot model
+        #=======================================================================
+        ax.plot(xser, pfunc(xser), label=aName, 
+            #marker=next(markers), markersize=2,
+            linewidth=0.2, **plotkwargs)
+        
+        #=======================================================================
+        # #confidence
+        #=======================================================================
+        if 'slope_conf' in res_d:
+            #get y values from each bound
+            bounds_d = dict()
+            for j, (m, b) in enumerate(zip(res_d['slope_conf'], res_d['inter_conf'])):
+                bounds_d[j] = [m * xi + b for xi in ax.get_xlim()]
+            
+            ax.fill_between(
+                ax.get_xlim(), bounds_d[0], bounds_d[1], 
+                interpolate=True, 
+                hatch=None, color=plotkwargs['color'], alpha=.3) #create a hatch between the points 'y' and the line 'depth'
+            
+        #=======================================================================
+        # observations
+        #=======================================================================
+        if 'regRes' in res_d:
+            regRes = res_d['regRes'] 
+            try:
+                pred_ols = regRes.get_prediction(sm.add_constant(xser))
+            except:
+                pred_ols = regRes.get_prediction(xser)
+ 
+            psmry_df = pred_ols.summary_frame()
+            
+            """
+            view(psmry_df.sort_values('mean'))
+            """
+            
+            
+            for label in ["obs_ci_lower", 'obs_ci_upper']:
+ 
+                ax.plot(xser.sort_values(), psmry_df[label].sort_values(), label='%s.%s' % (aName, label), 
+                #marker=next(markers), markersize=2,
+                    linewidth=0.4, linestyle='dashed', **plotkwargs)
+        
+ 
+        
+        return ax
+
     def regression(self, #get regressions from different modules and add to plot
                   df_raw,
                      xcoln='area',
@@ -46,6 +104,7 @@ class SimpleRegression(Plotr):
         """
         ax.set_ylabel(ycoln)
         ax.set_xlabel(xcoln)
+        ax.set_xlim(0, df_raw[xcoln].max())
         
         ax.scatter(df_raw[xcoln], df_raw[ycoln], color='black', marker='.', s=10)
         
@@ -70,22 +129,11 @@ class SimpleRegression(Plotr):
             
             
             #=======================================================================
-            # plot
+            # plot model
             #=======================================================================
-            """
-            plt.show()
-            """
-            
-            
-            ax.plot(df_raw[xcoln], pfunc(df_raw[xcoln]), label=aName, 
-                    #marker=next(markers), markersize=2,
-                    linewidth=0.2, **plotkwargs)
-            
-            # annotate
-
-            self.add_anno(res_d[aName], aName, ax=ax, xloc=xloc)
+            ax = self.plot_lm(df_raw, xcoln, ax, res_d, aName, plotkwargs, pfunc)
         
- 
+            self.add_anno(res_d[aName], aName, ax=ax, xloc=xloc)
             
             log.info('finished on %s w/ \n    %s'%(aName, res_d[aName]))
             
@@ -234,18 +282,22 @@ class SimpleRegression(Plotr):
     
     def sm_linregres(self,
                 df_raw,
-                intercept=False,
+                intercept=True,
                        xcoln='area',
                        ycoln='gvimp_gf',
- 
- 
- 
+
                ):
+        """
+        this seems to be the most powerfule module of those explored
+        provides confidence intervals of the model coefficients
+        and of the observations
+        documentation is bad
+        """
         
         log = self.logger.getChild('sm_linregres')
         
         #=======================================================================
-        # setup data
+        # setup dataTrue
         #=======================================================================
         df1 = df_raw.loc[:, [xcoln, ycoln]].dropna(how='any', axis=0)
         xtrain, ytrain=df1[xcoln].values, df1[ycoln].values
@@ -259,22 +311,34 @@ class SimpleRegression(Plotr):
         else:
             X = xtrain
         
+        #init the model
         model = sm.OLS(ytrain, X)
-        lm = model.fit()
-        print(lm.summary())
+        
+        #fit with data
+        regRes = model.fit()
+        log.info(regRes.summary())
         
         if intercept:
-            intercept, slope = lm.params[0], lm.params[1]
-            pfunc = lambda x:lm.predict(sm.add_constant(x))
+            res_d = {'intercept':regRes.params[0],
+             'slope':regRes.params[1],
+             'slope_conf':regRes.conf_int()[1],
+             'inter_conf':regRes.conf_int()[0],
+             }
+ 
+            
+            pfunc = lambda x:regRes.predict(sm.add_constant(x))
         else:
-            pfunc = lambda x:lm.predict(x)
-            intercept, slope = 0, lm.params[0]
-        #lm.predict(X)
-        """
-        pfunc(xtrain)
-        """
-        
-        return pfunc, {'rvalue':lm.rsquared, 'slope':slope, 'intercept':intercept}
+            pfunc = lambda x:regRes.predict(x)
+ 
+            res_d = {
+                'intercept':0,
+                 'slope':regRes.params[0],
+                 'slope_conf':regRes.conf_int()[0],
+                 'inter_conf':np.array([0,0]),
+                 }
+                        
+ 
+        return pfunc, {'rvalue':regRes.rsquared, 'stderr':regRes.bse[0],'regRes':regRes, **res_d}
         
     
     def add_anno(self,
@@ -283,7 +347,7 @@ class SimpleRegression(Plotr):
             ):
 
  
-        astr = '\n'.join(['%s = %.2f'%(k,v) for k,v in res_d.items()])
+        astr = '\n'.join(['%s = %.2f'%(k,v) for k,v in res_d.items() if isinstance(v, float)])
         
         anno_obj = ax.text(xloc, yloc,
                            '%s \n%s'%(label, astr),
