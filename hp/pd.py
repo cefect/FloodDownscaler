ecce2d858055a1048a1f54b79f683fd89217d74d
cefect <cefect@gmail.com>
2022-05-05 12:57:50 +0200

    get_bx_multiVal

--- hp/pd.py
+++ hp/pd.py
@@ -85,13 +85,43 @@ def get_bx_multiVal(df, #get boolean based on multi-column matching (single valu
         val_d, #{key:value} for making selection
         logicFunc = np.logical_and, #function for combining iterations
         baseBoolean=True, #where to start from
+        matchOn='columns', #here to perform the value matching
         log=None,
         ):
     
-    bx = pd.Series(baseBoolean, index=df.index) #start with nothing
+    #===========================================================================
+    # default
+    #===========================================================================
     
- 
-    meta_d= {'base':{'bx':bx.sum()}}
+    
+    
+    #===========================================================================
+    # construct the serach frame
+    #===========================================================================
+
+    if matchOn=='columns':
+        pass
+
+    elif matchOn=='index':
+        df_raw = df.copy()
+        #checks
+        mdex = df.index
+        assert isinstance(mdex, pd.MultiIndex)
+        
+        miss_l = set(val_d.keys()).difference(mdex.names)
+        assert len(miss_l)==0, miss_l
+        
+        df = mdex.to_frame().reset_index(drop=True)
+        
+        
+    else:
+        raise Error(matchOn)
+    
+    bx = pd.Series(baseBoolean, index=df.index) #start with nothing
+    meta_d= {'base':{'bx':bx.sum()}, 'logicFunc':logicFunc.__name__}
+    #===========================================================================
+    # execute serach
+    #===========================================================================
     for coln, val in val_d.items():
         if isinstance(val, list):
             new_bx = df[coln].isin(val)
@@ -100,20 +130,28 @@ def get_bx_multiVal(df, #get boolean based on multi-column matching (single valu
             raise Error('not implemented')
         else:
             new_bx = df[coln]==val
+            
         bx = logicFunc(bx,new_bx)
         
         meta_d[coln] = {'val':val, 'new_bx':new_bx.sum(), 'bx':bx.sum()}
         
+    #===========================================================================
+    # wrap
+    #===========================================================================
     if not log is None:
         log.info('on %s w/ %i matching vals got %i/%i for %s'%(
             str(df.shape), len(val_d),  bx.sum(), len(bx), val_d))
         
         mdf = pd.DataFrame.from_dict(meta_d).T
         log.debug(mdf)
+        
+    #reset the multindex
+    if matchOn=='index':
+        bx.index = df_raw.index
             
     return bx
         
-
+ 
 #===============================================================================
 # MULTINDEX-----------
 #===============================================================================
