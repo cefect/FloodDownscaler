af0a18638b2695b1120d13dd0f9d370b5d473cad
cefect <cefect@gmail.com>
2022-08-21 15:28:05 +0200

    Create prof.py

---
+++ hp/prof.py
@@ -0,0 +1,31 @@
+'''
+Created on Aug. 21, 2022
+
+@author: cefect
+'''
+import os, pstats, cProfile
+
+from definitions import wrk_dir
+
+def prof_simp(exe_str):
+    #build stats file
+    out_dir = os.path.join(wrk_dir, 'profile')
+    if not os.path.exists(out_dir): os.makedirs(out_dir)
+    stats_fp = os.path.join(out_dir,'stats_test')
+    
+    #execute profile
+    cProfile.run(exe_str, filename=stats_fp)    
+    print('finished profile to \n    %s'%stats_fp)
+    
+    #print profile stats
+    p = pstats.Stats(stats_fp)
+    p.strip_dirs().sort_stats('cumulative').print_stats(30)
+    
+    
+def stats_view(
+        stats_fp= r'C:\LS\10_IO\2207_fpolish\profile\stats_test',
+        ):
+    
+    p = pstats.Stats(stats_fp)
+    
+    p.strip_dirs().sort_stats('cumulative').print_stats(30)
