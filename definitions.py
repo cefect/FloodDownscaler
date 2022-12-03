b735841f1ba05b14f2daa75dad07afe8b1d38d0d
Seth Bryant <bryant.seth@gmail.com>
2022-12-02 21:40:37 -0700

    Create definitions.py

---
+++ definitions.py
@@ -0,0 +1,25 @@
+'''
+Created on Mar. 25, 2022
+
+@author: cefect
+
+I guess this is overwritten by local projects?
+'''
+import os
+ 
+
+
+
+src_dir = os.path.dirname(os.path.abspath(__file__))
+src_name = os.path.basename(src_dir)
+
+#location of logging configuration file
+logcfg_file=os.path.join(src_dir, r'coms\logger.conf')
+
+#default working directory
+wrk_dir = r'L:\10_IO\fdsc'
+
+ 
+
+ 
+
