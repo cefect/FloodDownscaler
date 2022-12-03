13c6297a6ec08a71c8c7393f4e586379aef6c4ac
Seth Bryant <bryant.seth@gmail.com>
2022-12-03 10:19:44 -0700

    update env

--- env/conda_create.bat
+++ env/conda_create.bat
@@ -1,4 +1,4 @@
 REM build the conda environment from the file
-call C:\Users\cefect\miniforge3\Scripts\activate.bat
-conda env create -f %~dp0..\environment.yml --name fdsc
+call setup_conda
+conda env create -f %ENV_FILE% --name %ENV_NAME%
 pause
