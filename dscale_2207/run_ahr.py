ac06c6dbedbe2fe4f99849424d9a9d845eb4fd62
cefect <cefect@gmail.com>
2023-01-27 19:03:50 +0100

    Update run_ahr.py

--- dscale_2207/run_ahr.py
+++ dscale_2207/run_ahr.py
@@ -8,7 +8,7 @@ downscaling ahr case study
 
 from fdsc.analysis.pipeline import run_dsc_vali, nicknames_d
 
-def aoi08_r32_1215_53(dryPartial_method='none', 
+def aoi08_r32_1215_53(method='none', 
                       run_name=None,
                       **kwargs):
     
@@ -16,9 +16,11 @@ def aoi08_r32_1215_53(dryPartial_method='none',
     
     NOTE: we clip  w/ AOI09 to remove some erroneous upstream results in teh validatoin
     """
+ 
+    assert method in nicknames_d, f'method {method} not recognized\n    {list(nicknames_d.keys())}'        
     
     if run_name is None:
-        run_name='121553_%s'%nicknames_d[dryPartial_method]
+        run_name='121553_%s'%nicknames_d[method]
     
     return run_dsc_vali(
         r'l:\10_IO\2207_dscale\ins\ahr\aoi08\fdsc\ahr_aoi08_r32_1215-0053_wse.tif',
@@ -27,14 +29,15 @@ def aoi08_r32_1215_53(dryPartial_method='none',
         sample_pts_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\bldgs\osm_buildings_aoi07_1114_poly_a50_cent_aoi09.geojson',
         aoi_fp=r'l:\02_WORK\NRC\202110_Ahr\04_CALC\aoi\aoi09_1221_r32.geojson', #target aligned
         
-        dsc_kwargs=dict(dryPartial_method=dryPartial_method),
+        dsc_kwargs=dict(method=method),
         run_name=run_name, proj_name='ahr_aoi08', index_coln='fid',
         **kwargs)
     
     
 if __name__=='__main__':
-    #aoi08_r32_1215_53(dryPartial_method='bufferGrowLoop')
-    #aoi08_r32_1215_53(dryPartial_method='costGrowSimple')
-    aoi08_r32_1215_53(dryPartial_method='none')
+    #aoi08_r32_1215_53(method='bufferGrowLoop')
+    #aoi08_r32_1215_53(method='costGrowSimple')
+    aoi08_r32_1215_53(method='schumann14')
+    #aoi08_r32_1215_53(method='none')
  
     print('done')
