b162e239313fb504dbe2a1c90ae21552cce584a1
cefect <cefect@gmail.com>
2023-03-01 18:50:32 +0100

    Update Ahr_run.py

--- dscale_2207/Ahr_run.py
+++ dscale_2207/Ahr_run.py
@@ -14,20 +14,22 @@ def runr(**kwargs):
     
     
 
-def aoi08_r32_1215_53(**kwargs):
-    
-    """t=53 for aoi08
-    
-    NOTE: we clip  w/ AOI09 to remove some erroneous upstream results in teh validatoin
-    """    
-    return runr(
-        wse2_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\fdsc\ahr_aoi08_r32_1215-0053_wse.tif',
-        dem1_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\r04\dem005_r04_aoi08_1210.asc',
-        wse1V_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\fdsc\ahr_aoi08_r04_1215-0053_wse.tif',
-        sample_pts_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\bldgs\osm_buildings_aoi07_1114_poly_a50_cent_aoi09.geojson',
-        aoi_fp=r'l:\02_WORK\NRC\202110_Ahr\04_CALC\aoi\aoi09_1221_r32.geojson', 
-        proj_name='ahr_aoi08',
-        **kwargs)
+#===============================================================================
+# def aoi08_r32_1215_53(**kwargs):
+#     
+#     """t=53 for aoi08
+#     
+#     NOTE: we clip  w/ AOI09 to remove some erroneous upstream results in teh validatoin
+#     """    
+#     return runr(
+#         wse2_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\fdsc\ahr_aoi08_r32_1215-0053_wse.tif',
+#         dem1_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\r04\dem005_r04_aoi08_1210.asc',
+#         wse1V_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\fdsc\ahr_aoi08_r04_1215-0053_wse.tif',
+#         sample_pts_fp=r'l:\10_IO\2207_dscale\ins\ahr\aoi08\bldgs\osm_buildings_aoi07_1114_poly_a50_cent_aoi09.geojson',
+#         aoi_fp=r'l:\02_WORK\NRC\202110_Ahr\04_CALC\aoi\aoi09_1221_r32.geojson', 
+#         proj_name='ahr_aoi08',
+#         **kwargs)
+#===============================================================================
 
 def ahr_aoi08_r32_0130_30(**kwargs):
     return runr(proj_name='ahr_aoi08_0130',
