a97e08db4b649df67542978269bd60824b2274ee
cefect <cefect@gmail.com>
2023-03-03 07:48:43 +0100

    add fred inun poly test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -75,6 +75,7 @@ proj_lib['fred01'] = {
     'wse1_rlayV_fp':os.path.join(src_dir, r'tests/data/fred01/vali/wse1_arV.tif'),
     'sample_pts_fp':os.path.join(src_dir, r'tests/data/fred01/vali/sample_pts_0109.geojson'),
     'samp_gdf_fp':os.path.join(src_dir, r'tests/data/fred01/vali/samps_gdf_0109.pkl'),
+    'inun_vlay_fp':os.path.join(src_dir, r'tests/data/fred01/vali/inun_vali1.geojson'),
     
     # post data
     'valiM_fp_d':{
