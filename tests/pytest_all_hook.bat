1c3a7a9d8222ab9cdb647b1d0575bb627fa75516
Seth Bryant <bryant.seth@gmail.com>
2023-03-28 12:18:22 -0600

    test hooks

--- tests/pytest_all_hook.bat
+++ tests/pytest_all_hook.bat
@@ -5,6 +5,9 @@ call "%~dp0../env/conda_activate.bat"
  
 :: call pytest
 ECHO starting tests in separate windows
-python -m pytest --maxfail=10 %~dp0 %SRC_DIR%\fperf\tests %SRC_DIR%\coms\hp\tests
+python -m pytest --maxfail=10 %~dp0 
+::%SRC_DIR%\fperf\tests %SRC_DIR%\coms\hp\tests
+
+if errorlevel 1 exit /b 1
  
 
