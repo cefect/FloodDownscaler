f6ce37ffc917cb7f34a5bbb8a4b299a11ef0b39c
Seth Bryant <bryant.seth@gmail.com>
2023-03-28 12:13:01 -0600

    Create pytest_all_hook.bat

---
+++ tests/pytest_all_hook.bat
@@ -0,0 +1,10 @@
+:: run pytest suite on all tests in the directory. and on the 
+
+:: activate the environment
+call "%~dp0../env/conda_activate.bat"
+ 
+:: call pytest
+ECHO starting tests in separate windows
+python -m pytest --maxfail=10 %~dp0 %SRC_DIR%\fperf\tests %SRC_DIR%\coms\hp\tests
+ 
+
