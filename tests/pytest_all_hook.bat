0e431b9d78abfc5f71c242be5e954d43fef09146
Seth Bryant <bryant.seth@gmail.com>
2023-03-28 12:22:54 -0600

    Update pytest_all_hook.bat

--- tests/pytest_all_hook.bat
+++ tests/pytest_all_hook.bat
@@ -1,4 +1,4 @@
-:: run pytest suite on all tests in the directory. and on the 
+:: run pytest suite on all tests in the directory. setup for pre-push hook
 
 :: activate the environment
 call "%~dp0../env/conda_activate.bat"
@@ -8,6 +8,7 @@ ECHO starting tests in separate windows
 python -m pytest --maxfail=10 %~dp0 
 ::%SRC_DIR%\fperf\tests %SRC_DIR%\coms\hp\tests
 
+:: return pytest's error code
 if errorlevel 1 exit /b 1
  
 
