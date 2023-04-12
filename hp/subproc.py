12e514fc70d2215b31d94929412f1e1d5757d18b
Seth Bryant <bryant.seth@gmail.com>
2023-04-12 17:57:48 -0600

    Update subproc.py
    sub_cmd

--- hp/subproc.py
+++ hp/subproc.py
@@ -2,12 +2,18 @@
 subprocess helpers
 
 """
-import subprocess
+import subprocess, logging
 
 
-def sub_cmd(cmd, **kwargs):
+def sub_cmd(cmd, logger = None, **kwargs):
     """execute a command as a subprocess"""
-    print(f'with cmd={cmd}')
+    # defaults
+    if logger is None: logger= logging.getLogger()
+    log = logger.getChild('subprocess')
+
+    #execute
+    log.debug(f'with cmd={cmd}')
+
     err_str = ''
     with subprocess.Popen(cmd,
                           stdout=subprocess.PIPE,
@@ -18,10 +24,10 @@ def sub_cmd(cmd, **kwargs):
         for line in proc.stdout:
             if line in ['', '\n']: continue  # skip empty lines
             # print(f'    {line}', end='') #print stdout
-            print('    %s' % line.replace('\n', ''))  # log stdout
+            log.debug('    %s' % line.replace('\n', ''))  # log stdout
 
         for line in proc.stderr:
-            print(line.replace('\n', ''))
+            log.error(line.replace('\n', ''))
             err_str += line
 
             # check the error status
@@ -29,6 +35,6 @@ def sub_cmd(cmd, **kwargs):
     if (proc.returncode != 0) or (err_str != ''):
         raise RuntimeError(msg + '\n' + err_str)
 
-    print(msg)
+    log.info(msg)
 
     return proc
